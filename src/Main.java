827ea30b587b98080543cb5e19b3bc4d9a53156c
Emil <emweiss@tsn.at>
2024-10-23 10:43:23 +0200

    weiter Klasse Zwerg ausgearbeitet

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        
+
+
+        System.out.println(ThreadLocalRandom.current().nextInt(1,2));
     }
 }
--- src/Zwerg.java
+++ src/Zwerg.java
@@ -1,5 +1,52 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+
+
 public class Zwerg extends Charakter{
     public Zwerg(String name, boolean spezialfähigkeitAktiv, int lebenspunkte) {
         super(name, spezialfähigkeitAktiv, lebenspunkte);
     }
+
+    private int damage;
+
+    public void angreifen(Charakter gegner){
+        if(isSpezialfähigkeitAktiv()){
+            damage = ThreadLocalRandom.current().nextInt(15, 26)-ThreadLocalRandom.current().nextInt(5,10);
+            gegner.setLebenspunkte(lebenspunkte-damage);
+        } else{
+            int damage = ThreadLocalRandom.current().nextInt(20, 26);
+            gegner.setLebenspunkte(lebenspunkte-damage);}
+    }
+
+    public void spezialFähigkeit(){
+        if(!isSpezialfähigkeitAktiv()){
+            if(lebenspunkte<50){
+            spezialfähigkeitAktiv = true;
+
+            if(lebenspunkte<50&&lebenspunkte>20)
+            {
+                if(ThreadLocalRandom.current().nextInt(1,11)<=3){
+                    damage=2*damage;
+
+                }else damage = damage/2;
+            }
+
+                if(lebenspunkte<20&&lebenspunkte>10)
+                {
+                    if(ThreadLocalRandom.current().nextInt(1,6)==1){
+                        damage=2*damage;
+
+                    }else damage = damage/2;
+                }
+
+
+
+
+            }
+        } else if (isSpezialfähigkeitAktiv()) {
+            spezialfähigkeitAktiv = false;
+            lebenspunkte=lebenspunkte-10;
+        }
+    }
+
 }
